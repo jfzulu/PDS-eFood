26cce73f132c8523bb0ecde03d418243c9f9d1b5
Jose Fernando Zuluaga Ortiz <jtgame17@gmail.com>
2023-09-08 15:26:15 -0500

    Primer Commit

---
+++ src/test/java/com/example/eFood/EFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eFood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
